ec7b89e17b0c8d20ffd76851487be5a0c32ce725
Suraj Shrestha <sshrestha@salesforce.com>
2022-07-25 00:56:45 -0400

    Adding graphql, firebase libraries

---
+++ app/graphql/graphql-client.js
@@ -0,0 +1,30 @@
+import { ApolloClient, HttpLink, from } from '@apollo/client'
+import { InvalidationPolicyCache } from 'apollo-invalidation-policies'
+import { onError } from '@apollo/client/link/error'
+import crashlytics from '@react-native-firebase/crashlytics'
+import { SERVER_URL } from '@env'
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors) {
+		graphQLErrors.forEach((error) => crashlytics().recordError(new Error(error.message)))
+	}
+
+	if (networkError) {
+		crashlytics().recordError(new Error(networkError.message))
+	}
+})
+
+const cache = new InvalidationPolicyCache({
+	invalidationPolicies: {
+		timeToLive: 86400000,
+	},
+})
+
+const httpLink = new HttpLink({ uri: SERVER_URL })
+
+const client = new ApolloClient({
+	link: from([errorLink, httpLink]),
+	cache,
+})
+
+export default client
--- app/index.js
+++ app/index.js
@@ -2,6 +2,11 @@ import { persistor, store } from 'app/store'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import { ApolloProvider } from '@apollo/client'
+import crashlytics from '@react-native-firebase/crashlytics'
+import auth from '@react-native-firebase/auth'
+import firebase from '@react-native-firebase/app'
+import GraphqlClient from './graphql/graphql-client'
 import App from './navigation'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import * as Utils from '@utils'
@@ -10,13 +15,15 @@ Utils.setupLayoutAnimation()
 
 const NTApp = () => {
 	return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<SafeAreaProvider>
-					<App />
-				</SafeAreaProvider>
-			</PersistGate>
-		</Provider>
+		<ApolloProvider client={GraphqlClient}>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<SafeAreaProvider>
+						<App />
+					</SafeAreaProvider>
+				</PersistGate>
+			</Provider>
+		</ApolloProvider>
 	)
 }
 
