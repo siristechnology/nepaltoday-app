aa251d21a1b4bb2867bf8223c6df3a349841851a
Suraj Shrestha <syuraj@gmail.com>
2022-08-20 10:47:34 -0400

    Update prepare_release.sh

--- prepare_release.sh
+++ prepare_release.sh
@@ -9,6 +9,7 @@ ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
 IC_LAUNCHER_BACKGROUND_FILE=./android/app/src/main/res/values/ic_launcher_background.xml
 
 sed -i '' -e 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
+grep -C 2 applicationId $BUILD_GRADLE
 sed -i '' -e 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
 sed -i '' -e 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
 
