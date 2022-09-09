54afbdfc4f47ba665984f292b94cc2eea0463306
Suraj Shrestha <syuraj@gmail.com>
2022-09-09 00:01:15 -0400

    auto refresh page when app brought to foreground from background

--- app/screens/ScreenContainer/Index.js
+++ app/screens/ScreenContainer/Index.js
@@ -1,10 +1,13 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
+import { AppState } from 'react-native'
 import { useDebounceCallback } from '@react-hook/debounce'
 import { useFocusEffect } from '@react-navigation/native'
 import { SafeAreaView } from '@components'
 import { BaseStyle } from '@config'
 
 const ScreenContainer = (props) => {
+	const appState = useRef(AppState.currentState)
+
 	useFocusEffect(
 		useDebounceCallback(
 			useCallback(() => {
@@ -14,6 +17,20 @@ const ScreenContainer = (props) => {
 			true,
 		),
 	)
+
+	useEffect(() => {
+		const subscription = AppState.addEventListener('change', (nextAppState) => {
+			if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
+				props.handleRefresh()
+			}
+
+			appState.current = nextAppState
+		})
+
+		return () => {
+			subscription.remove()
+		}
+	}, [])
 
 	return (
 		<SafeAreaView style={BaseStyle.safeAreaView} edges={['right', 'top', 'left']}>
